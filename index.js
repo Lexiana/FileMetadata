0e93d2b4c3148aa105e0007fb52514c95108ee34
Lexiana <schaller.lexi@gmail.com>
2024-08-02 18:23:39 +0200

    feat: post file

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+// mutler for uploading files
+var multer = require('multer');
+// path for file paths
+const path = require('path');
 
 var app = express();
+// where files are uploaded
+const upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// endpoint for uploading files
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if(!req.file) {
+    return res.status(400).send('No file uploaded');
+  }
+  const { originalname: name, size, mimetype: type } = req.file;
+  res.json({ name, size, type });
 });
 
 
